814feb5b54f1f09cf6caf72e42064e8c67ef8e67
Alexandr Smirnov <newlinefu@gmail.com>
2020-12-08 15:56:59 +0300

    Main page v_0.5 (Without second modal, body, tests)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React from 'react'
+import {Box, Grommet} from 'grommet'
+import Header from './components/main_page/header/Header'
 
+const theme = {
+    global: {
+        font: {
+            family: 'Roboto',
+            size: '18px',
+            height: '20px',
+        }
+    }
+}
 
 export default function App() {
-  return (
-    <div>
+    return (
+        <Grommet theme={theme}>
 
-    </div>
-  )
+            <Header/>
+        </Grommet>
+    )
 }
