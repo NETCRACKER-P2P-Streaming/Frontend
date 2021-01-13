954fc77784ca44e220b182076c9e1af5b88ac6b9
Alexandr Smirnov <newlinefu@gmail.com>
2021-01-13 16:52:57 +0300

    Sign done (main page without refactor)

--- src/API/user_api.js
+++ src/API/user_api.js
@@ -1,6 +1,5 @@
 import {request} from './api'
 
-//test
 export function register(formData) {
     return request
         .post('/api/v1/user/registration', formData)
@@ -13,7 +12,6 @@ export function register(formData) {
         })
 }
 
-//test
 export function auth(formData) {
     return request
         .post('/api/v1/auth/login', formData)
@@ -27,7 +25,6 @@ export function auth(formData) {
         })
 }
 
-//test
 export function getUser(username) {
     return request
         .get(`/api/v1/user/${username}`)
