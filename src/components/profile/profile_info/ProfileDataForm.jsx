463cc88bab9a437e2a225481183444713a910a60
Roza Zhaisanova <57352835+RozaZhaisanova@users.noreply.github.com>
2021-03-24 15:29:25 +0300

    первый работающий пут-запрос)

--- src/components/profile/profile_info/ProfileDataForm.jsx
+++ src/components/profile/profile_info/ProfileDataForm.jsx
@@ -1,77 +1,51 @@
-import React, { useState } from 'react'
-import { putUserAttributes } from '../../../API/user_api';
-import { Cookies } from 'react-cookie'
+  
+import React, {useState} from 'react'
+import axios from 'axios';
 
-export default
-    function PersonDataForm(props) {
-    const [errorMessage, setErrorMessage] = useState(undefined)
+export default 
+    function PersonDataForm() {
+ 
+  const [form, setForm] = useState({
+    name: ''
+  });
+  let handleChange = e => {
+   setForm({   ...form,
+    [e.target.name]: e.target.value });
+  };
 
-    const [form, setForm] = useState({
-        name: ''
-    });
-    let handleChange = e => {
-        setForm({
-            ...form,
-            [e.target.name]: e.target.value
-        });
-    };
+  let handleSubmit = event => {
+    event.preventDefault();
 
-    async function onSubmit() {
-        try {
-            await putUser(form)
-        } catch (err) {
-            setErrorMessage(err.message)
-        }
-    }
-    function putUser(userData) {
-        return async dispatch => {
-            try {
-                const validUserData = {
-                    username: userData.username,
-                    password: userData.password,
-                    userAttributes: [
-                        {
-                            name: "name",
-                            value: userData.firstName
-                        },
-                        {
-                            name: "custom:linkImage",
-                            value: userData.linkImage
-                        },
-                        {
-                            name: "custom:description",
-                            value: userData.description
-                        },
-                        {
-                            name: "family_name",
-                            value: userData.lastName
-                        },
-                        {
-                            name: "email",
-                            value: userData.email
-                        }
-                    ]
-                }
-                const cookies = new Cookies()
-                const result = await putUserAttributes(validUserData, cookies.get('accessToken'))
-                if (!result) {
-                    throw new Error('Save failed. Try again later')
-                }
-            } catch (err) {
-                return Promise.reject(err)
-            }
-        }
-    }
+    const user = {
+        "userAttributes": [
+            {
+                "name": "name",
+                "value": form.name
+            }]}
+       
+
+    axios.put(`http://localhost:9090/api/v1/users`,  user ,{  headers: {
+        withCredentials: true,
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer `+'eyJraWQiOiJaMkk2YXBocHVoOFd6d3EyTFJTQUR4d0ZtY1p2Y0ZEbnNITjIrd3dqVWQ4PSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiIwZjc4ODc0YS1hNzllLTQ1ZWYtOTk2Yy1kNjUzOTcyMmVhMWQiLCJjb2duaXRvOmdyb3VwcyI6WyJVU0VSIl0sImV2ZW50X2lkIjoiZGQwNjliYTEtMTNkNS00NzhjLWE0MmUtNjE2ZjdlNTgyNDUwIiwidG9rZW5fdXNlIjoiYWNjZXNzIiwic2NvcGUiOiJhd3MuY29nbml0by5zaWduaW4udXNlci5hZG1pbiIsImF1dGhfdGltZSI6MTYxNjU4ODUzOSwiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLmV1LXdlc3QtMi5hbWF6b25hd3MuY29tXC9ldS13ZXN0LTJfaDlyRGpVTjdyIiwiZXhwIjoxNjE2NTkyMTM5LCJpYXQiOjE2MTY1ODg1MzksImp0aSI6IjRiYTkxZTg0LTdhNGQtNDZkMi1hNWRkLTllODg4OTAwNzNhMSIsImNsaWVudF9pZCI6IjY4b2dmMjI0aDIwNjIya21rMHEycW40MnJnIiwidXNlcm5hbWUiOiJyb3phMjEwNCJ9.F0Y8uBu4FV97wxJyMjLJV4xqnyRwEwdHov_7TDQxqs7xQrPHy2DIDv-8pS7jy5YxAvlc04r0zKqxnfK_Trqorn108i811LAT7RHrur07AdT3Fk2SzCM9loyWC8kz5k_4UWK9FEVRRu07myav4Ry-W36okNuV8ab4ER0NhlhjXzmseu81NzpItZ1VbBdbWqTiNVphbDr2pdNwYl-SSNYIcvKSYQIYpdyQCsga_p2MUZciQP-qrOlrqjt0pSBygyNVjO-l9fHrdGyftDwCPFBDFx5avhmk500ebIjCVev4UCI1f7ma67nndQp_MQCBYpr_f_pSnyESU0LUlteQEBky3w'
+    }})
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      })
+  }
+
+  
     return (
-        <div>
-            <form onSubmit={onSubmit}>
-                <label>
-                    Person Name:
-                        <input value={form.name} name="name" onChange={handleChange} />
-                </label>
-                <button type="submit">Add</button>
-            </form>
-        </div>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Person Name:
+            <input   value={form.name} name="name" onChange={handleChange} />
+          </label>
+          <button type="submit">Add</button>
+        </form>
+      </div>
     )
-
+  
 }
