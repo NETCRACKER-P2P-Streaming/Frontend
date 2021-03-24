2b61f973136b26be9cece19a25c8aa26a25273af
Roza Zhaisanova <57352835+RozaZhaisanova@users.noreply.github.com>
2021-03-24 19:27:36 +0300

    edit

--- src/components/profile/profile_info/ProfileDataForm.jsx
+++ src/components/profile/profile_info/ProfileDataForm.jsx
@@ -3,9 +3,9 @@ import {Cookies} from 'react-cookie'
 import React, {useState} from 'react'
 import { putUserData } from '../../../API/profile_api'
 import { saveProfile } from '../../../redux/reducers/profile_reducer';
+import ProfileDataFormSave from './ProfileDataFormSave';
 
-export default 
-    function PersonDataForm(isOwner) {
+const ProfileDataForm=({profile,isOwner})=> {
         let [editMode, setEditMode] = useState(false);
 
   const [form, setForm] = useState({
@@ -15,19 +15,14 @@ export default
    setForm({   ...form,
     [e.target.name]: e.target.value });
   };
-/*   const onSubmit = (formData) => {
-    saveProfile(formData).then(
+   const handleSubmit = (formData) => {
+    saveProfile(form).then(
         () => {
             setEditMode(false);
         }
     );
-} */
-  let handleSubmit = (event) => {
-    event.preventDefault();
-    saveProfile(form)
+} 
 
-
-  }
 
   
     return (
@@ -38,8 +33,35 @@ export default
             <input   value={form.name} name="name" onChange={handleChange} />
           </label>
           <button type="submit">Add</button>
+          <div> { editMode
+                    ? <ProfileDataFormSave />
+                    : <ProfileData goToEditMode={() => {setEditMode(true)} } profile={profile} isOwner={isOwner}/> }
+</div>
         </form>
       </div>
     )
   
 }
+
+const ProfileData = ({profile, isOwner, goToEditMode}) => {
+    //debugger;
+    return <div>fff</div>
+    /* let userAtt = profile.userAttributes.reduce((acc, att) => {
+        acc[att.name] = att.value
+        return acc
+      }, {})
+    return <div>
+        {isOwner && <div><button onClick={goToEditMode}>edit</button></div>}
+        <div>
+            <b>Nickname</b>: {profile.username}
+        </div>
+        <div>
+            <b>Email</b>: {userAtt['email'] }
+        </div>
+       
+    
+   
+    </div> */
+}
+export default 
+    ProfileDataForm
